6d7d3348e918ce1a9328cf841b4190600ff3689c
Florent BENOIT <fbenoit@redhat.com>
2021-09-17 12:39:49 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+
+function hello() {
+    console.log('Hello World');
+}
+
+     function wait(ms) {
+         return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+     });
+   }
+
+async function loop() {
+         for(let i = 0; i < 50; i++) {
+            hello();
+      await wait(1000);
+  }
+}
+
+loop();
